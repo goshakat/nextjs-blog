0b42293d97e6e8d50be8dc84363eaaa42e399da2
Alexander Khramov <hramovsaku@icloud.com>
2023-03-29 19:20:58 +0300

    404 custom page update

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,7 @@
-
+import utilStyles from '../styles/utils.module.css';
 
 export default function Custom404() {
-    return <h1 className="error">404 - Page Not Found</h1>;
+    return <section className={utilStyles.box404}>
+        <h1 className={utilStyles.error}>404 - Page Not Found</h1>
+        </section>
   }
